e77238dd3364eebda2a7f8bf419b3d90c99f92a4
RainYu <790634623@qq.com>
2018-09-02 10:27:19 +0800

    nothing

--- taobao-v1.js
+++ taobao-v1.js
@@ -1,1 +1,9 @@
-var message = document.querySelectorAll('.message>ul>li');
+computedScreen();
+
+function computedScreen() {
+    var navTop = document.getElementsByClassName('nav-top')[0];
+    var contentWrapper = document.getElementsByClassName('content-wrapper')[0];
+    var contentNav = document.getElementsByClassName('content-nav')[0];
+    var contentAll = document.getElementsByClassName('content-all')[0];
+
+}
