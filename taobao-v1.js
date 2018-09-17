eb568c92f5404131bd9c3a2df772fdeb4b84339b
RainYu <790634623@qq.com>
2018-09-17 15:18:28 +0800

    add fixed nav

--- taobao-v1.js
+++ taobao-v1.js
@@ -1,9 +1,21 @@
-computedScreen();
+// computedScreen();
 
-function computedScreen() {
-    var navTop = document.getElementsByClassName('nav-top')[0];
-    var contentWrapper = document.getElementsByClassName('content-wrapper')[0];
-    var contentNav = document.getElementsByClassName('content-nav')[0];
-    var contentAll = document.getElementsByClassName('content-all')[0];
+// function computedScreen() {
+//     var navTop = document.getElementsByClassName('nav-top')[0];
+//     var contentWrapper = document.getElementsByClassName('content-wrapper')[0];
+//     var contentNav = document.getElementsByClassName('content-nav')[0];
+//     var contentAll = document.getElementsByClassName('content-all')[0];
 
-}
+// }
+$(function() {
+    $(window).scroll(function() {
+        var navTop = $('.nav-top').height();
+        var conWrapper = $('.content-wrapper').height();
+        if ($(window).scrollTop() >= navTop + conWrapper) {
+            $('.content1').css('display', 'block');
+        } else {
+            $('.content1').css('display', 'none');
+
+        }
+    })
+});
